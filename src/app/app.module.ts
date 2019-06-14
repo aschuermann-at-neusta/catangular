5cdccb010398cc158a0d7d75dc1a6a60fca0eb81
aschuermann <aschuermann@neusta.de>
2019-06-14 08:11:44 +0200

    feature/restart1: Initial clean-up

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,7 @@
-import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { CatApiModule } from './cat-api/cat-api.module';
-import { BreedSelectionComponent } from './modules/random-cat-image/breed-selection/breed-selection.component';
-import { RandomCatImageModule } from './modules/random-cat-image/random-cat-image.module';
 
 @NgModule({
     declarations: [
@@ -13,9 +9,7 @@ import { RandomCatImageModule } from './modules/random-cat-image/random-cat-imag
     ],
     imports: [
         BrowserModule,
-        AppRoutingModule,
-        CatApiModule,
-        RandomCatImageModule
+        AppRoutingModule
     ],
     bootstrap: [
         AppComponent
--- src/app/cat-api/breed/cat-breed.ts
+++
@@ -1,4 +0,0 @@
-export interface CatBreed {
-    id: string;
-    name: string;
-}
